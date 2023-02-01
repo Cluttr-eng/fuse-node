6786860d8f86d5b9b705974c3c8cbc1d962cb54a
AdibFuse <106388522+AdibFuse@users.noreply.github.com>
2023-02-01 11:04:01 +0000

    Update fuse node (#21)

--- src/helpers.ts
+++
@@ -1,8 +0,0 @@
-export const getHeaderValue = (headers: any, key: string): string => {
-  for (let header of Object.keys(headers)) {
-    if (header.toLowerCase() === key) {
-      return headers[header]!;
-    }
-  }
-  return null;
-};
